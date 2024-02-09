c6bda6b7f3ee4700cb631b7dbfa13dcf2961d314
Dobromir Kirov <kirov0407@gmail.com>
2024-02-09 18:29:08 +0200

    feat: extended def select element with reusable and custom one

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,28 @@
+import { SelectHTMLAttributes } from "react";
+import { useAirports } from "../../hooks/useAirports";
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  onChangeHandler: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  placeholderText: string;
+}
+
+const Select = ({ onChangeHandler, placeholderText, ...rest }: SelectProps) => {
+  const airports = useAirports();
+
+  return (
+    <select {...rest} onChange={onChangeHandler} defaultValue={0}>
+      <option value={0} disabled>
+        {placeholderText}
+      </option>
+      {airports.map((airport) => {
+        return (
+          <option key={airport.id} value={airport.id}>
+            {airport.title}
+          </option>
+        );
+      })}
+    </select>
+  );
+};
+
+export default Select;
