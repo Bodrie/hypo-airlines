f64ea4a218f1f2089a3363e20af52966d1227c94
Dobromir Kirov <kirov0407@gmail.com>
2024-02-11 13:49:12 +0200

    fix: get req and hook for all bookings

--- src/hooks/useAllBookings.tsx
+++ src/hooks/useAllBookings.tsx
@@ -2,11 +2,11 @@ import { useState, useEffect } from "react";
 import { BookingListT } from "../types/types";
 import { getAllBookings } from "../services/getAllBookings";
 
-export const useAllBookings = () => {
+export const useAllBookings = (pageIndex = 0, pageSize = 5) => {
   const [allBookings, setAllBookings] = useState<BookingListT[]>([]);
 
   useEffect(() => {
-    getAllBookings().then((res) => setAllBookings(res));
+    getAllBookings(pageIndex, pageSize).then((res) => setAllBookings(res.list));
   }, []);
 
   return allBookings;
--- src/services/getAllBookings.ts
+++ src/services/getAllBookings.ts
@@ -1,8 +1,11 @@
-export const getAllBookings = async () => {
+export const getAllBookings = async (pageIndex: number, pageSize: number) => {
   const API = import.meta.env.VITE_APP_API;
+  const API_TOKEN = import.meta.env.VITE_APP_API_TOKEN;
 
   try {
-    const response = await fetch(`${API}/bookings`);
+    const response = await fetch(
+      `${API}/bookings?pageIndex=${pageIndex}&pageSize=${pageSize}&authToken=${API_TOKEN}`
+    );
 
     if (!response.ok) {
       return response.text();
